6ef6f7e1a496b59859d5c72569a5e6e2237368ad
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2020-03-04 18:24:49 +0900

    Initial Commit

---
+++ RocketServer/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  RocketServer
+//
+//  Created by 赤堀雅司 on 4/3/20.
+//  Copyright © 2020 赤堀雅司. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
